a1cb7c34a84d9b33a7a6e0a221d4bf58ed039c10
Alexandre Cotton <alex59219@sfr.fr>
2015-09-03 10:37:13 +0200

    ajout de class CreerDossier

---
+++ src/CreerDossier.java
@@ -0,0 +1,31 @@
+import java.io.File;
+
+
+public class CreerDossier {
+	public CreerDossier(String chemin){
+		
+		String dossier;
+		File f = new File(chemin);
+		
+			for(int i = chemin.length()-1 ; i >= 0 ; i--){
+				String c = "" + chemin.charAt(i);
+				if(c.equals("/")){
+					dossier = chemin.substring(0, i);
+					System.out.println(dossier);
+					File d = new File(dossier);
+					if(!d.exists()){
+						System.out.println("le chemin n'est pas correct");
+					}
+					else{
+						f.mkdir(); 
+						System.out.println("dossier cree");
+					}
+					break;
+				}
+
+			}
+
+		}
+		
+	}
+
